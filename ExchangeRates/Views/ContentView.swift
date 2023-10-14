46bda0fe90743cb449b1d908e21235397269dd51
MatheusFranceschini <masuzena@gmail.com>
2023-10-14 16:34:23 -0300

    Adicionando a listagem de flutuação de moedas e taxas

---
+++ ExchangeRates/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Matheus Franceschini on 14/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatesFluctuationView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
